edf4a1baf5d40da1e2b3f73fcf50fd47425f397c
alejogs4 <alejogs4@gmail.com>
2017-10-22 12:48:51 -0500

    code format

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-const currentPosition=()=>{
-    if(!navigator.geolocation){
-        console.log("The geolocation is not supported in this browser")
-    }
-    let position=navigator.geolocation.getCurrentPosition((positionData)=>{
-        getcurrentWeather(positionData.coords.altitude,positionData.coords.longitude)
+const currentPosition = () => {
+  if (!navigator.geolocation) {
+    console.log("The geolocation is not supported in this browser");
+  }
+  let position = navigator.geolocation.getCurrentPosition(
+    positionData => {
+      getcurrentWeather(
+        positionData.coords.altitude,
+        positionData.coords.longitude
+      );
     },
-    (err)=>{
+    err => {
+      console.log("The users don't let us to use the geolocation");
+    }
+  );
+};
+const getcurrentWeather = (altitude, longitude) => {
+  let xr = new XMLHttpRequest();
+};
 
-    })
-}
-const getcurrentWeather=(altitude,longitude)=>{
-    let xr=new XMLHttpRequest()
-    
-}
-
-window.onload=currentPosition()
+window.onload = currentPosition();
