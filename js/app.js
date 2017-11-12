f8bb14a5b97f44799f914ccb434cf42cea6e2bd3
alejogs4 <alejogs4@gmail.com>
2017-11-12 18:40:08 -0500

    display weather data

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@ const currentPosition = () => {
   let position = navigator.geolocation.getCurrentPosition(
     positionData => {
       let apiKey = "1ebd32963d194fcd9bb9af04df7591b5";
-      getcurrentWeather(
-        positionData.coords.latitude,
-        positionData.coords.longitude,
-        apiKey
-      );
+      getcurrentWeather(positionData.coords.latitude,positionData.coords.longitude,apiKey);
     },
     err => {
       console.log("The users don't let us to use the geolocation");
@@ -17,6 +13,7 @@ const currentPosition = () => {
   );
 };
 const getcurrentWeather = (latitude, longitude, key) => {
+  const apiMapsKey='AIzaSyCZ6vNHaC5-XHVOmBuEvmOVLPhRSUgbXco'
   let xr = new XMLHttpRequest();
   xr.open(
     "GET",
@@ -27,37 +24,25 @@ const getcurrentWeather = (latitude, longitude, key) => {
     console.log(weatherData);
     let currentWeather = weatherData.main;
     let weatherLocale = weatherData.weather;
-    displayWeatherData(
-      currentWeather.temp,
-      currentWeather.temp_max,
-      currentWeather.temp_min,
-      currentWeather.humidity,
-      weatherLocale
-    );
+    let image=`https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${latitude},${longitude}&heading=151.78&pitch=-0.76&key=${apiMapsKey}`
+    displayWeatherData(currentWeather.temp,currentWeather.temp_max,
+      currentWeather.temp_min,currentWeather.humidity,weatherData,image);
   };
   xr.send();
 };
-const displayWeatherData = (
-  temp,
-  maxTemp,
-  minTemp,
-  humidityTemp,
-  localeWeather
-) => {
-  console.log(temp, maxTemp, minTemp, humidityTemp);
-  let weatherContainer = document.createElement("div");
+const displayWeatherData = (temp,maxTemp,minTemp,humidityTemp,localeWeather,img) => {
+  console.log(temp, maxTemp, minTemp, humidityTemp,localeWeather);
 
-  let img = {
-    source:
-      temp <= 18
-        ? "./assets/img/nubes.png"
-        : temp >= 20 ? "./assets/img/sol.png" : "",
-    alt: "clima"
-  };
+  let date=new Date()
+  let hours=date.getHours()
+  let minutes=date.getMinutes()
+  
+  let weatherContainer = document.createElement("div");
   weatherContainer.innerHTML = `
     <div class="weather_data_container">
-      <img src="${img.source}" alt="${img.alt}">
-      <p>${temp}</p>
+      <img src="${img}">
+      <p>La temperatura en ${localeWeather.name} a las ${hours}:${minutes} es</p>
+      <h2>${temp} grados centigrados</h2>
     </div>
   `;
 
